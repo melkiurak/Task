939b4639cd33c5ce6386ef1c69f8f35635d3469c
Danila Ribka <melkiurak3209@gmail.com>
2025-01-04 13:25:01 +0200

    Add Task and Remove

--- task.js
+++ task.js
@@ -1,12 +1,36 @@
-function addTask() {
-    const inputValue = document.getElementById('taskInput');
-    const listTask = document.getElementById('taskList');
-    if(inputValue.value.trim() !== '') {
-        const newLi = document.createElement('li');
-        listTask.appendChild(newLi);
-        newLi.innerHTML = inputValue.value;
-        inputValue.value = '';
-    } else{
-        console.log('Вы ничего не ввели')
+const list = document.getElementById('taskList');
+let actions = JSON.parse(localStorage.getItem('actions')) || [];
+const time = document.getElementById('inputTime');
+
+function addTask(){
+    time.style.display = 'block';
+    if(time.value == ''){
+        console.log('Пажалуйста выберите время');
+    } else { 
+        const inputData = document.getElementById('taskInput').value;
+        actions.push({task:inputData, time:time.value});
+        localStorage.setItem('actions', JSON.stringify(actions));
+        showTask();
     }
 }
+const showTask = () => {
+    list.innerHTML = '';
+    actions.forEach((action, index) => {
+        const task = document.createElement('li');
+        const time = document.createElement('span');
+        const buttonDelete = document.createElement('button');
+        task.innerHTML = action.task;
+        time.innerHTML = action.time;
+        buttonDelete.innerHTML = 'Удалить';
+        buttonDelete.onclick = () => deleteTask(index);
+        list.appendChild(task);
+        list.appendChild(time);
+        list.appendChild(buttonDelete);
+    });
+}
+function deleteTask(index) {
+    actions.splice(index, 1);
+    localStorage.setItem('actions', JSON.stringify(actions));
+    showTask();
+};
+showTask();
